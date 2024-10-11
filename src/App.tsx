077aa52408467f87b3499fd1bf6baf94c5878ede
OleksandrP <oleksandr.p@wellnessliving.com>
2024-10-11 13:31:00 -0400

    Added environment files and updated scripts in package.json

--- src/App.tsx
+++ src/App.tsx
@@ -24,6 +24,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <p>{import.meta.env.VITE_EXAMPLE_KEY}</p>
     </>
   );
 }
